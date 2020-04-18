8d205addd4efa61bf200076789eaebf5dff18630
tobyklauder <tobyklauder@hotmail.com>
2020-04-18 10:30:01 -0700

    hover over objects of importance and see what they are!

---
+++ ludumdareone/Assets/Scripts/ovenscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+public class ovenscript : MonoBehaviour
+{
+    public GameObject fridgetext; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        fridgetext.SetActive(false); 
+        
+    }
+
+    private void Update()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(this.transform.position);
+        pos.x = pos.x - 33;
+        pos.y = pos.y - 20; 
+        fridgetext.transform.position = pos; 
+    }
+
+    private void OnMouseOver()
+    {
+        fridgetext.SetActive(true); 
+    }
+
+    private void OnMouseExit()
+    {
+        fridgetext.SetActive(false); 
+    }
+}
