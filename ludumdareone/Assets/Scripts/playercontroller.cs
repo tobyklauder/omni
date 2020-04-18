1f4cf1b333e8e69fc02868a2cf20c1bbe59004b2
tobyklauder <tobyklauder@hotmail.com>
2020-04-17 20:01:22 -0700

    implement basic a*

---
+++ ludumdareone/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= (float)0.30;
+            this.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += (float)0.30;
+            this.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.y += (float)0.30;
+            this.transform.position = position;
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            Vector3 position = this.transform.position;
+            position.y -= (float)0.30;
+            this.transform.position = position; 
+        }
+    }
+}
