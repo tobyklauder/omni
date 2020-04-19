c11d3c68ed5f0fccccec83bd8ed97ae0c41c8c0f
Adeline Braun <adeline.braun@digipen.edu>
2020-04-19 14:13:09 -0700

    Implemented music and scenechange script

---
+++ ludumdareone/Assets/Scripts/changescene.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void loadMenu()
+    {
+        SceneManager.LoadScene("mainmenu", LoadSceneMode.Single);
+    }
+ 
+    public void loadGame()
+    {
+        SceneManager.LoadScene("mainscene", LoadSceneMode.Single);
+    }
+    public void loadGameOver()
+    {
+        SceneManager.LoadScene("gameover", LoadSceneMode.Single);
+    }
+
+}
---
+++ ludumdareone/Assets/Scripts/musicmanager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicmanager : MonoBehaviour
+{
+    public AudioClip music;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = music;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
